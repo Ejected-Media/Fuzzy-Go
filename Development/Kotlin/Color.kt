b4f0dbc6c6e12ad89fad04d5fd4beac771b32fdd
Kyle2501 <kyle2501@gmail.com>
2025-12-03 10:30:25 -0700

    Create Color.kt

---
+++ Development/Kotlin/Color.kt
@@ -0,0 +1,26 @@
+package com.ejectedmedia.fuzzygo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// --- Standard Fuzzy Colors (Existing) ---
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// --- FAIRY FLOSS PALETTE 🍬 ---
+// Based on https://sailorhg.github.io/fairyfloss/
+object FairyFloss {
+    val Background = Color(0xFF5a5475)   // The iconic soft plum background
+    val Surface    = Color(0xFF3b3a32)   // Slightly darker for cards/inputs
+    val Text       = Color(0xFFf8f8f0)   // Creamy white text
+    
+    val Pink       = Color(0xFFff857f)   // "Salmon" - Primary Action
+    val Purple     = Color(0xFFc5a3ff)   // "Pale Purple" - Headers/Accents
+    val Yellow     = Color(0xFFfff352)   // "Kryptonite" - Warnings/Highlights
+    val Mint       = Color(0xFFc2ffdf)   // "Mint" - Success/Done
+    val Cyan       = Color(0xFF4688cc)   // "Holo Blue" - Links/Info
+}
